fd7a0f5b14e037a5789f3e8cca09a9acd2637d36
N1l0c <colin.freeth@gmail.com>
2015-12-16 07:02:41 +0000

    Basic LCD functionality moved to class based structure

---
+++ Sensor.py
@@ -0,0 +1,43 @@
+import urllib
+import datetime as dt
+
+# The sensor class contains current and previous measurements,
+# the measurement time as a datetime object
+# as well as location and ip address strings
+class Sensor(object):
+
+    def __init__(self, location, ipaddress):
+        self.time = dt.datetime.now()
+        self.temp = 0.0
+        self.humi = 0
+        self.loc = location
+        self.ip = ipaddress
+
+    def now(self):
+        # Set last measurement values
+        self.templast = self.temp
+        self.humilast = self.humi
+        self.timelast = self.time
+        # Download page sources
+        sockt = urllib.urlopen('http://' + self.ip + '/temp')
+        srct = sockt.read()
+        sockt.close()
+        self.time = dt.datetime.now()  # Set new measurement time
+        sockh = urllib.urlopen('http://' + self.ip + '/humidity')
+        srch = sockh.read()
+        sockh.close()
+        # Split, strip strings and set values
+        tempf = srct.split(' ', 1)[1].strip(' F')
+        self.humi = int(srch.split(' ', 1)[1].strip('%'))
+        self.temp = (float(tempf) - 32) / 1.8  # Convert temperature to celcius
+
+    # The delta function returns the difference between the last measurement
+    # and the current measurement as a tuple
+    def delta(self):
+        tempdelta = self.templast - self.temp
+        humidelta = self.humilast - self.humilast
+        return tempdelta, humidelta
+
+    # Returns a nicely datetime string
+    def timeFormat(self, layout='%a %d %b %H:%M'):
+        return self.time.strftime(layout)
--- lcd_display.py
+++ lcd_display.py
@@ -1,12 +1,50 @@
+
+from string import Template
+
 import Adafruit_CharLCD as LCD
+import Sensor
 
-import SensorMeasure as poll
+##############################
 
-# General structure
+# LCD setup and functions
+lcd = LCD.Adafruit_CharLCDPlate()
+
+# Custom characters
+lcd.create_char(0, [24, 24, 3, 4, 4, 4, 3, 0])  # Create a degrees character
+lcd.create_char(1, [4, 14, 21, 4, 4, 0, 0, 0])  # Up arrow
+lcd.create_char(2, [0, 0, 4, 4, 21, 14, 4, 0])  # Down arrow
+degrees = '\x00'
+up = '\x01\x01\x01'
+down = '\x02\x02\x02'
+
+# String templates for LCD
+measStr = Template('TMP:${tempval}' + degrees + ' RH:${humival}%')
+measDict = {'tempval': '---',
+            'humival': '---'}
+
+##############################
+
+# Instantiate sensor and show blank readings
+room1 = Sensor.Sensor('Front Room', '192.168.1.57')
+lcd.message(measStr.substitute(measDict))
+lcd.message('\n' + room1.timeFormat())
+
+# Main loop
 while True:
-    # Get measurement data
     try:
-        measurement = poll.readall()
+        room1.now()  # Get current readings
+        # Format values for LCD and store in relevant dictionary entry
+        measDict['tempval'] = '{:.1f}'.format(room1.temp)
+        measDict['humival'] = '{}'.format(room1.humi)
+
+        # Update display if change
+        if (room1.delta()[0] or room1.delta()[1]) != 0:
+            lcd.clear()
+            lcd.message(measStr.substitute(measDict))
+            lcd.message('\n' + room1.timeFormat())
+        else:
+            lcd.home()
+            lcd.message('\n' + room1.timeFormat())
 
     # If wifi goes down notify on screen
     except IOError:
